e920f374a94b7fd5cd437e61a549c4581716172a
Derek <dyin00@stuy.edu>
2019-03-11 22:19:35 -0400

    used tested parititon as basis

--- Quick.java
+++ Quick.java
@@ -1,3 +1,22 @@
 public class Quick{
-  
+public static int quickSelect(int[] data, int k){
+  int random = (int)(Math.random() * (end - start) + start);
+  int temp = data[random];
+  data[random] = data[end];
+  data[end] = temp;
+  int pivot = temp;
+  while (start <end){
+    while (data[start] < pivot){
+      start++;
+    }
+    while (data[end] > pivot){
+      end--;
+    }
+    if (start < end){
+      int temp = data[start];
+      data[start] = data[end];
+      data[end] = temp;
+    }
+  }
+  return end;
 }

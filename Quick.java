a3da06ffb62abbf37280240c233cf3f9c0dbdd84
Derek <dyin00@stuy.edu>
2019-03-12 22:51:29 -0400

    merge

--- Quick.java
+++ Quick.java
@@ -11,12 +11,17 @@ public static int quickSelect(int[] data, int k){
   for (int i =start; i < up; i++){
     if (data[i] <= pivot){
       low++;
-      int temp1 = data[low];
+      int temp1 = data[low];// swap if int at index is smaller or equal to the pivot (sort in terms of larger, smaller or equal)
       data[low] = data[i];
       data[i] = temp1;
     }
   }
   int temp2 = data[low+1];
-  data[low+1] = 
-  return end;
+  data[low+1] = data[end];
+  data[end] = temp2;
+  return low +1;
+}
+public static void main(String[] args) {
+  int[]ary = { 2, 10, 15, 23, 0,  5};
+}
 }
